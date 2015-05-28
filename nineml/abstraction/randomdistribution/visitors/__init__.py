5ac1da15f05dac667fa9b2f00591330f23c5e906
Tom Close <tclose@oist.jp>
2015-05-28 16:02:28 +0900

    removing redundant visitors

--- nineml/abstraction/randomdistribution/visitors/__init__.py
+++ nineml/abstraction/randomdistribution/visitors/__init__.py
@@ -1,3 +1,2 @@
 from .interface_inferer import RandomDistributionInterfaceInferer
-from .queryer import RandomDistributionQueryer
 from .xml import RandomDistributionXMLLoader, RandomDistributionXMLWriter
