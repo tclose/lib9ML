719e09a85d64e1c126748cf735332229d58b9797
Tom Close <tclose@oist.jp>
2015-09-20 23:41:15 +1000

    renamed multi_dynamics to multicomponent

--- nineml/user/__init__.py
+++ nineml/user/__init__.py
@@ -4,11 +4,11 @@ from .network import Network
 from .population import Population, PositionList, Structure
 from .selection import Selection, Concatenate
 from .port_connections import (
-    AnalogPortConnection, EventPortConnection, ReducePortConnection)
+    AnalogPortConnection, EventPortConnection)
 from .projection import Projection
 from .component import (Property, Component, Definition,
                         Prototype, resolve_reference, write_reference,
                         Initial, DynamicsProperties,
                         ConnectionRuleProperties, RandomDistributionProperties,
                         Quantity)
-from .multi_dynamics import MultiDynamics, MultiCompartment
+from .multicomponent import MultiDynamics, MultiCompartment
