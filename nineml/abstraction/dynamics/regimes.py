dab468bb23ea51b9bd326f9e04133079ee9d9266
Tom Close <tclose@oist.jp>
2015-10-15 19:07:42 +1100

    added no_time_derivatives method for finding state variables without a time derivative in a regime

--- nineml/abstraction/dynamics/regimes.py
+++ nineml/abstraction/dynamics/regimes.py
@@ -363,3 +363,11 @@ class Regime(BaseALObject, ContainerObject):
     @property
     def name(self):
         return self._name
+
+    def no_time_derivatives(self, state_variables):
+        """
+        Returns the state variables from the list provided that don't have
+        time derivatives in the current regime
+        """
+        return (sv for sv in state_variables
+                if sv.name not in self.time_derivative_variables)
