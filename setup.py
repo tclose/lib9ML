5f991370fc4f466d790ee7fc42b0e784f27cdeee
Tom Close <tom.g.close@gmail.com>
2017-11-20 09:53:20 +1100

    updated release number and author, email details

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lib9ML",
-    version="1.0dev",
+    name="nineml",
+    version="0.1",
     package_data={'nineml': ['examples/AL/demos/*.py',
                              'examples/AL/sample_components/*.py']},
     packages=find_packages(),
     author="Andrew P. Davison, Eilif Muller, Mike Hull, Thomas G. Close",
     # add your name here if you contribute to the code
-    author_email="nineml-users@incf.org",
+    author_email="tom.g.close@gmail.com",
     description=(
         "A tool for reading, writing and generally working with 9ML files."),
     long_description=open("README.rst").read(),
     license="BSD 3 License",
     keywords="computational neuroscience modeling interoperability XML",
-    url="http://nineml.incf.org",
+    url="http://nineml.net",
     classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Console',
                  'Intended Audience :: Science/Research',
